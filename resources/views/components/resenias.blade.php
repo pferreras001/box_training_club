fffcf7df03246844a982ecba1ed3324ba3c8f2f0
endika <endikasier@gmail.com>
2021-06-28 20:10:45 +0200

    reseñas malisimas
    No he conseguido nada makina

---
+++ resources/views/components/resenias.blade.php
@@ -0,0 +1,14 @@
+<div id="wpac-google-review"></div>
+<script type="text/javascript">
+wpac_init = window.wpac_init || [];
+wpac_init.push({widget: 'GoogleReview', id: 31137, place_id: 'ChIJC68IisGvUQ0ROVi9jr1NX0U', view_mode: 'list'});
+(function() {
+    if ('WIDGETPACK_LOADED' in window) return;
+    WIDGETPACK_LOADED = true;
+    var mc = document.createElement('script');
+    mc.type = 'text/javascript';
+    mc.async = true;
+    mc.src = 'https://embed.widgetpack.com/widget.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(mc, s.nextSibling);
+})();
+</script>
