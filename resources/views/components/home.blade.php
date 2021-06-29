a931d5cfa4a065bfb28e5193a25c8046b38c4ac1
Pablo <pfadarraga@gmail.com>
2021-06-29 10:17:44 +0200

    home v1

--- resources/views/components/home.blade.php
+++ resources/views/components/home.blade.php
@@ -1,3 +1,13 @@
-<div>
-    <!-- Breathing in, I calm body and mind. Breathing out, I smile. - Thich Nhat Hanh -->
-</div>
+<section class="section section__home">
+  <div class="home__container">
+    <div class="home__left">
+      <div>
+        <h1>Box Training Club Donostia</h1>
+        <h2>STRENGTHEN SPIRITS THROUGH MIND & PHYSIQUE</h2>
+      </div>
+    </div>
+    <div class="home__right">
+      <?php require('svg/home/escudo.svg')?>
+    </div>
+  </div>
+</section>
