97082828fbfa09033c19aafd9963f7ee4ffc9b1c
Pablo <pfadarraga@gmail.com>
2021-07-06 14:26:27 +0200

    entrenamiento_personal v01

--- resources/views/components/entrenamiento_personal.blade.php
+++ resources/views/components/entrenamiento_personal.blade.php
@@ -1,3 +1,9 @@
-<div>
-    <!-- Because you are alive, everything is possible. - Thich Nhat Hanh -->
-</div>
+<section class="section section__entrenamiento_personal">
+  <div class="entrenamiento_personal__container">
+    <div class="left"></div>
+    <div class="right">
+      <h1>¿NECESITAS UN ENTRENAMIENTO <span>PERSONALIZADO</span>?</h1>
+      <h3><a href="">Contacta con nosotros para saber más...</a></h3>
+    </div>
+  </div>
+</section>
