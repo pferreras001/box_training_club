2eb52482681d5a2e20cfbd7d642a5ee83ef7b6bb
Pablo <pfadarraga@gmail.com>
2021-07-07 11:31:25 +0200

    noticias v01

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -16,24 +16,23 @@
 
   <x-empresas/>
 
-  <section class="section section__blog">
-      <div id="entradas">
+  <section class="section section__noticias">
+    <div class="noticias__container container">
+      <h1 class="section-title">Noticias ></h1>
+      <div class="noticias__entrys">
         @foreach($blogentrys as $blogentry)
-            <div class ="sm:grid grid-cols-2 gap-20 w-4/5 mx-auto py-15 border-b border-gray-200">
-                <h2 class="text-yellow-300 font-bold text-5xl pb-4">
-                        {{$blogentry->titulo}}
-                </h2>
-                <div class="text-white">
-                    <img src="{{asset('/images/'.$blogentry->image)}}" height="300" width="300">
-                </div>
-                <div> 
-                    <span class="text-yellow-300">
-                        Escrito por <span class="font-bold italic text-white-500">{{$blogentry->autor}}</span> el dia {{date('d-m-Y',strtotime($blogentry->fecha))}}
-                    </span>
-                    <a  href="{{route('show_entry',['id' =>$blogentry->id])}}" class="uppercase text-white font-extrabold py-4 px-8 rounded-3xl"> seguir leyendo</a>
-                </div>
-            </div>
+        <div class="noticias__entry">
+          <div class="top">
+            <img src="{{asset('/images/'.$blogentry->image)}}" alt="{{ $blogentry->titulo }}">
+            <h2>{{ $blogentry->titulo }}</h2>
+          </div>
+          <div class="bottom">
+            <div><p>{{ $blogentry->texto }}</p></div>
+            <div><a href="{{route('show_entry',['id' =>$blogentry->id])}}">Leer más...</a></div>
+          </div>
+        </div>
         @endforeach
+      </div>
     </div>
   </section>
 
