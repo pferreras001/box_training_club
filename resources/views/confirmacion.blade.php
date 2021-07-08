8cc237956c392c2dd8f84b93b49cc50a6266747b
endika <endikasier@gmail.com>
2021-07-08 18:25:03 +0200

    chorraditas post reunion

--- resources/views/confirmacion.blade.php
+++ resources/views/confirmacion.blade.php
@@ -2,7 +2,7 @@
 <htlm lang="es">
 <head>
     <meta charset="utf-8">
-    <h1>¡Bienvenido a Box Family!</h1>
+    <h1>¡Bienvenido a Box Famility!</h1>
 </head>
 <body>
     <p>Bienvenido a Box Family, para registrar su cuenta, haga clic en el siguiente enlace: </p>
