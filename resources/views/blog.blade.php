79d1c4438a2db57d6dd1d6e3b8af8a1ec37b0276
Pablo <pfadarraga@gmail.com>
2021-07-14 10:55:27 +0200

    blog v01

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -3,6 +3,33 @@
 @section('section')
 
 <section class="section section__blog">
+  <div class="blog__container container">
+    <form action="{{route('blog')}}" method="POST">
+      @csrf
+      <select id="etiquetas" name="etiquetas" onchange="this.form.submit()">
+        <option selected="selected">Todos</option>
+        @foreach($etiquetas as $etiqueta)
+            <option>{{$etiqueta->etiqueta}}</option>
+        @endforeach
+      </select>
+    </form>
+
+    <div class="noticias__entrys">
+      @foreach($blogentrys as $blogentry)
+        <div class="noticias__entry">
+          <div class="top">
+            <img src="{{asset('/images/blog/'.$blogentry->image)}}" alt="{{ $blogentry->titulo }}">
+            <h2>{{ $blogentry->titulo }}</h2>
+          </div>
+          <div class="bottom">
+            <div><p>{{ $blogentry->texto }}</p></div>
+            <div><a href="{{route('show_entry',['id' =>$blogentry->id])}}">Leer más...</a></div>
+          </div>
+        </div>
+      @endforeach
+    </div>
+  </div>
+
 <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
     <div class="w-4/5 m-auto text-center">
         <div class="py-15 border-b border-gray-200">
