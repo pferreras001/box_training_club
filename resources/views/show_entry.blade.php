3262961d778ceb2fef41e8de3e33fe67ba3979e9
Pablo <pfadarraga@gmail.com>
2021-07-14 12:23:38 +0200

    blog_entry v01

--- resources/views/show_entry.blade.php
+++ resources/views/show_entry.blade.php
@@ -3,12 +3,19 @@
 @section('section')
 
 <section class="section section__show_entry">
-    <h1>{{$entry->titulo}}</h1>
-    <span class="text-yellow-300">
-                Escrito por <span class="font-bold italic text-white-500">{{$entry->autor}}</span> el dia {{date('d-m-Y',strtotime($entry->fecha))}}
-    </span><br><br>
-    <p>{{$entry->texto}}</p>
-    <br><br>
+  <div class="show_entry__container container">
+    <div class="show_entry__entry">
+      <p>
+        <img class="show_entry__img" src="{{asset('/images/blog/'.$entry->image)}}">
+        <h1>{{$entry->titulo}}</h1>
+        {!! nl2br(e($entry->texto)) !!}
+      </p>
+    </div>
+    <div class="show_entry__button">
+      <button class="btn nav__btn" onclick="location.href='{{ route('blog') }}'">Volver</button>
+    </div>
+  </div>
+
     @if(session('tipo')=='admin')
     <a  href="{{route('edit_entry',['id' =>$entry->id])}}" class="uppercase font-extrabold py-4 px-8 rounded-3xl"> Editar</a>
     <a  href="{{route('delete_entry',['id' =>$entry->id])}}" class="uppercase font-extrabold py-4 px-8 rounded-3xl"> Borrar</a>
