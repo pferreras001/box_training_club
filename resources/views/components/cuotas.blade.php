fdec3715d467ad8c8039965e8c6cf953733adf4a
Pablo <pfadarraga@gmail.com>
2021-08-06 09:59:10 +0200

    cuotas v01

--- resources/views/components/cuotas.blade.php
+++ resources/views/components/cuotas.blade.php
@@ -1,3 +1,96 @@
-<div>
-    <!-- When there is no desire, all things are at peace. - Laozi -->
-</div>
+<section class="section section__cuotas">
+  <div class="cuotas__container">
+    <div data-aos="fade-up" class="cuotas__top">
+      <div>
+        <h1>¿QUIERES UNIRTE A <span>BOX TRAINING CLUB</span>?</h1>
+        <h3>Matrícula de inscripción 50€</h3>
+      </div>
+    </div>
+  </div>
+  <div class="cuotas__tarifas">
+    <div class="cuotas__tarifa cuotas__tarifa--jab">
+      <div data-aos="fade-right" class="cuotas__left">
+        <ul>
+          <li><span>Mensual: </span>65€ + matrícula</li>
+          <li><span>Trimestral: </span>195€ (matrícula gratuita)</li>
+          <li><span>Semestral: </span>370€ (matrícula gratuita)</li>
+          <li><span>Anual: </span>702€ (matrícula gratuita)</li>
+        </ul>
+      </div>
+      <div data-aos="fade-left" class="cuotas__right cuotas__right--jab">
+        <div class="cuotas__right__top">
+          <div>
+            <h1>JAB</h1>
+            <h2>10 Clases / mes</h2>   
+          </div>       
+        </div>
+        <div class="cuotas__right__bottom">   
+          <h3>INICIACIÓN</h3>       
+        </div>
+      </div>
+    </div>
+    <div class="cuotas__tarifa cuotas__tarifa--cross">
+      <div data-aos="fade-right" class="cuotas__left">
+        <ul>
+          <li><span>Mensual: </span>75€ + matrícula</li>
+          <li><span>Trimestral: </span>225€ (matrícula gratuita)</li>
+          <li><span>Semestral: </span>427€ (matrícula gratuita)</li>
+          <li><span>Anual: </span>810€ (matrícula gratuita)</li>
+        </ul>
+      </div>
+      <div data-aos="fade-left" class="cuotas__right cuotas__right--cross">
+        <div class="cuotas__right__top">
+          <div>
+            <h1>CROSS</h1>
+            <h2>13 Clases / mes</h2>   
+          </div>       
+        </div>
+        <div class="cuotas__right__bottom">   
+          <h3>MEDIO</h3>       
+        </div>
+      </div>
+    </div>
+    <div class="cuotas__tarifa cuotas__tarifa--hook">
+      <div data-aos="fade-right" class="cuotas__left">
+        <ul>
+          <li><span>Mensual: </span>85€ + matrícula</li>
+          <li><span>Trimestral: </span>255€ (matrícula gratuita)</li>
+          <li><span>Semestral: </span>484€ (matrícula gratuita)</li>
+          <li><span>Anual: </span>918€ (matrícula gratuita)</li>
+        </ul>
+      </div>
+      <div data-aos="fade-left" class="cuotas__right cuotas__right--hook">
+        <div class="cuotas__right__top">
+          <div>
+            <h1>HOOK</h1>
+            <h2>16 Clases / mes</h2>   
+          </div>       
+        </div>
+        <div class="cuotas__right__bottom">   
+          <h3>AVANZADO</h3>       
+        </div>
+      </div>
+    </div>
+    <div class="cuotas__tarifa cuotas__tarifa--tyson">
+      <div data-aos="fade-right" class="cuotas__left">
+        <ul>
+          <li><span>Mensual: </span>100€ + matrícula</li>
+          <li><span>Trimestral: </span>300€ (matrícula gratuita)</li>
+          <li><span>Semestral: </span>570€ (matrícula gratuita)</li>
+          <li><span>Anual: </span>1080€ (matrícula gratuita)</li>
+        </ul>
+      </div>
+      <div data-aos="fade-left" class="cuotas__right cuotas__right--tyson">
+        <div class="cuotas__right__top">
+          <div>
+            <h1>TYSON</h1>
+            <h2>Ilimitado</h2>   
+          </div>       
+        </div>
+        <div class="cuotas__right__bottom">   
+          <h3>ÉLITE</h3>       
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
