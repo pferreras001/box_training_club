ada7186e78ed74e2b7d2443ed7c3d5b71fff59fb
Pablo <pfadarraga@gmail.com>
2021-08-29 01:05:23 +0200

    contacto fixes

--- resources/views/components/contacto.blade.php
+++ resources/views/components/contacto.blade.php
@@ -3,7 +3,6 @@
     <div data-aos="fade-right" class="contacto__form__left">
       <form method="POST" action="{{route('contacto')}}">
       @csrf
-        <span class="errmsg errmsg__contacto">*Rellene todos los campos del formulario</span><br>
         <input type="email" name="email" required="true" placeholder="Email"><br>
           @isset($asunto)
             <input type="text" name="asunto" required="true" value="{{$asunto}}"><br>
@@ -35,10 +34,10 @@
       </div>
       <div class="contacto__yellow_box contacto__yellow_box__contacto">
         <ul>
-          <li><a href="">Madalena Jauregiberri 4 - Donostia 20014</a></li>
-          <li><a href="">+34 600 46 69 67</a></li>
-          <li><a href="">+34 843 63 15 10</a></li>
-          <li><a href="">info@boxtrainingclub.com</a></li>
+          <li><a href="https://www.google.com/maps/place/Box+Training+Club/@43.309273,-1.967805,17z/data=!4m5!3m4!1s0x0:0x455f4dbd8ebd5839!8m2!3d43.3092732!4d-1.9678051?hl=es">Madalena Jauregiberri 4 - Donostia 20014</a></li>
+          <li><a href="tel:+34600466967">+34 600 46 69 67</a></li>
+          <li><a href="tel:+34843631510">+34 843 63 15 10</a></li>
+          <li><a href="mailto:info@boxtrainingclub.com">info@boxtrainingclub.com</a></li>
         </ul>
       </div>     
     </div>
