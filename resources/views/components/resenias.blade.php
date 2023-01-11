52d2a51f6437a185814f237273e9cc9675fc1a06
Pablo <pfadarraga@gmail.com>
2023-01-11 07:42:56 +0100

    fixes

--- resources/views/components/resenias.blade.php
+++ resources/views/components/resenias.blade.php
@@ -2,7 +2,60 @@
   <div class="resenias__container container">
     <h1 class="section-title">{{ __('opiniones.opiniones') }} ></h1>
     <div id="wpac-google-review"></div>
-    <div class="resenias__btn"><button class="btn nav__btn" onclick="location.href='https://maps.google.com/?cid=4998799587943471161'">{{ __('opiniones.ver') }}</button></div>
+
+    <div class="resenias__box">
+      <div class="resenias__box__head">
+        <img src="img/resenias/1.png">
+        <div>Carmen Figueroa</div>
+      </div>
+      <ul>
+        <li><img src="https://maps.gstatic.com/consumer/images/icons/2x/ic_star_rate_14.png"></li>
+        <li><img src="https://maps.gstatic.com/consumer/images/icons/2x/ic_star_rate_14.png"></li>
+        <li><img src="https://maps.gstatic.com/consumer/images/icons/2x/ic_star_rate_14.png"></li>
+        <li><img src="https://maps.gstatic.com/consumer/images/icons/2x/ic_star_rate_14.png"></li>
+        <li><img src="https://maps.gstatic.com/consumer/images/icons/2x/ic_star_rate_14.png"></li>
+      </ul>
+      <p>
+        Para mi el mejor sitio donde he entrenado. No me gusta nada el deporte y aquí me encantó. Beñat un profesor de 10. Una pena que no pueda ir ahora porque no me coinciden horarios, pero prometo volver!!
+      </p>
+    </div>
+
+    <div class="resenias__box">
+      <div class="resenias__box__head">
+        <img src="img/resenias/2.png">
+        <div>Iker Malles</div>
+      </div>
+      <ul>
+        <li><img src="https://maps.gstatic.com/consumer/images/icons/2x/ic_star_rate_14.png"></li>
+        <li><img src="https://maps.gstatic.com/consumer/images/icons/2x/ic_star_rate_14.png"></li>
+        <li><img src="https://maps.gstatic.com/consumer/images/icons/2x/ic_star_rate_14.png"></li>
+        <li><img src="https://maps.gstatic.com/consumer/images/icons/2x/ic_star_rate_14.png"></li>
+        <li><img src="https://maps.gstatic.com/consumer/images/icons/2x/ic_star_rate_14.png"></li>
+      </ul>
+      <p>
+        Totalmente recomendable! Son muy cercanos y tienes mucha variedad de entrenamientos. Me han ayudado a motivarme por el deporte y a empezar a cambiar de hábitos. Gracias!
+      </p>
+    </div>
+
+    <div class="resenias__box">
+      <div class="resenias__box__head">
+        <img src="img/resenias/3.png">
+        <div>Ibai Marin</div>
+      </div>
+      <ul>
+        <li><img src="https://maps.gstatic.com/consumer/images/icons/2x/ic_star_rate_14.png"></li>
+        <li><img src="https://maps.gstatic.com/consumer/images/icons/2x/ic_star_rate_14.png"></li>
+        <li><img src="https://maps.gstatic.com/consumer/images/icons/2x/ic_star_rate_14.png"></li>
+        <li><img src="https://maps.gstatic.com/consumer/images/icons/2x/ic_star_rate_14.png"></li>
+        <li><img src="https://maps.gstatic.com/consumer/images/icons/2x/ic_star_rate_14.png"></li>
+      </ul>
+      <p>
+        El día que decidí empezar a entrenar en Box Training Club, me cambió la vida, no es una frase hecha es mi realidad. Llegué arrastrando una lesión de rodilla, dolores y movilidad reducida de la misma. Hoy es el día en el que casi ni me acuerdo de la lesión. El ambiente es muy cercano y la gran variedad de tipos de entrenamiento, disciplinas, horarios… hacen de este lugar, un lugar del que quieres ser participe. Tu autoestima, tu mente, tu cuerpo, te lo agradecerán. Si el fin del ser humano es buscar la felicidad, apúntate y verás!!!
+      </p>
+    </div>
+
+    <a href="https://www.google.com/maps/place/Box+Training+Club/@43.3092732,-1.9678051,17z/data=!4m7!3m6!1s0x0:0x455f4dbd8ebd5839!8m2!3d43.3092732!4d-1.9678051!9m1!1b1" target="_blank"><div class="resenias__btn"><button class="btn nav__btn">{{ __('opiniones.ver') }}</button></div></a>
+
   </div>
 <script type="text/javascript">
 wpac_init = window.wpac_init || [];
@@ -21,73 +74,4 @@ wpac_init.push({widget: 'GoogleReview', id: 31137, place_id: 'ChIJC68IisGvUQ0ROV
   $(".wp-google-review").addClass("custom__review");
 </script>
 
-
-<?php
-
-/**
- * Get google reviews
- * @return array Google reviews data
- */
-function get_google_reviews(){
-
-  // URL to fetch
-  $google_api = 'https://maps.googleapis.com/maps/api/place/details/json?placeid=ChIJC68IisGvUQ0ROVi9jr1NX0U&sensor=true&key=31137';
-
-  // Fetch reviews with cURL
-  $ch = curl_init();
-
-  curl_setopt($ch, CURLOPT_HEADER, 0);
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-  curl_setopt($ch, CURLOPT_URL, $google_api);
-
-  $response = curl_exec($ch);
-  curl_close($ch);
-
-  // JSON decode the text to associative array
-  return json_decode($response, 'assoc');
-}
-
-// Get reviews
-$g_response = get_google_reviews();
-
-// See if we got some reviews
-if ($g_response && $g_response['result'] && $g_response['result']['reviews']) {
-  // Loop through the reviews
-  foreach ($g_response['result']['reviews'] as $review) {
-    // Output HTML for reviews
-    ?>
-    <dl>
-      <dt> date </dt>
-      <dd><?php echo $review['time'] ?></dd>
-      <dt> rating </dt>
-      <dd><?php echo $review['rating'] ?></dd>
-      <dt> name </dt>
-      <dd><?php echo $review['author_name'] ?></dd>
-      <dt> content </dt>
-      <dd><?php echo $review['text'] ?></dd>
-    </dl>
-    <?php
-  }
-}
-?>
-
-<!-- OPTIONAL: Some styles to make things look nicer -->
-<style>
-  dl {
-    display: flex;
-    flex-wrap: wrap;
-    font: 1rem/1.4 sans-serif;
-    max-width: 500px;
-    margin: 2em auto 3em;
-  }
-  dt, dd {
-    margin: .5em 0;
-  }
-  dt {
-    flex: 1 1 25%;
-  }
-  dd {
-    flex: 1 1 75%;
-  }
-</style>
 </section>
